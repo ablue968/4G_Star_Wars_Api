caad5cb87fd19e1ccb202c4c54ff761f9db146eb
ablue968 <ablue968@gmail.com>
2021-03-23 19:55:46 +0000

    falta corregir el like

--- src/components/heartButton.js
+++ src/components/heartButton.js
@@ -1,14 +1,26 @@
-import React, { useContext} from "react";
+import React, { useContext,useState,useEffect} from "react";
 import { Context } from "../store/index.js";
 
 export default function(props) {
     const {store, actions} = useContext(Context);
-    
+
+    const [like, setLike] = useState("far fa-heart")
+
     const handleClick = () => {
-         actions.addFavorites(props.name)
-     }
-     
+        if (props.name in store.favorites) {
+            setLike("fas fa-heart");
+        }
+        if (like == "far fa-heart") {
+            setLike("fas fa-heart");
+            actions.addFavorites(props.name);
+        } else {
+            setLike("far fa-heart");
+            actions.deleteFavorites(props.name);
+        }
+    };
+
+
     return (
-        <button onClick= {handleClick} type="button" className="btn heart"><i className="corazon far fa-heart">♥</i></button>
+        <button onClick= {handleClick} type="button" className="btn btn-outline-warning"><i className={like}></i></button>
     )
 }
